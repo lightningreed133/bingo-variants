9105da9b037b6e5ebdf12962b4f8faba3119cf84
cirion02 <34488326+cirion02@users.noreply.github.com>
2023-10-22 22:36:31 +0200

    Add all variants page

---
+++ variant-box-generator.js
@@ -0,0 +1,50 @@
+
+function variantToElement(variant){
+    let div = document.createElement("div");
+    div.className = "variantBox";
+
+    let name = document.createElement("h3");
+    name.className = variant.color;
+    name.innerText = variant.name;
+    div.appendChild(name);
+
+    let credit = document.createElement("div");
+    credit.className = "credit";
+    credit.innerText = "Creator: " + variant.credit;
+    div.appendChild(credit);
+
+    let description = document.createElement("div");
+    description.className = "description";
+    description.innerText = "Description: " + variant.description;
+    div.appendChild(description);
+
+    if (variant.notes) {
+        let notes = document.createElement("div");
+        notes.className = "notes";
+        notes.innerText = "Additional Notes: " + variant.notes;
+        div.appendChild(notes);
+    }
+
+    if (variant.external_links) {
+        for (let linkData of variant.external_links){
+            let link = document.createElement("div");
+            link.className = "link";
+            link.innerHTML = `${linkData.name}: <a href="${linkData.link}">${linkData.link}</a>`;
+            div.appendChild(link);
+        }
+    }
+
+    return div;
+}
+
+const VARIANT_COLORS = ["Black", "Purple", "Blue", "Green", "Orange", "Red"];
+
+function addAllVariants(){
+    for (let color of VARIANT_COLORS) {
+        for (let variant of variant_list_data){
+            if (!(variant.color == color)) continue;
+
+            document.getElementById("holder").appendChild(variantToElement(variant))
+        }
+    }
+}
--- variant-list.js
+++ variant-list.js
@@ -1,8 +1,8 @@
-let data = [
+let variant_list_data = [
     {
         name: "Lockout",
         credit: "Initial draft made by Flesh, updated/maintained by Tim, jacks, marlin, truegen, amber, Spirity, aco, crab, and others.",
-        desciption: "Two players compete to finish a board of 25 squares. First to tick 13 objectives wins. If one person ticks an objective, the other may not tick it. Prologue cutscenes are not skippable. Standard custom progression rules apply. Additional Notes: The main competitive gamemode and form of Celeste bingo. Highly conducive to strategy.",
+        description: "Two players compete to finish a board of 25 squares. First to tick 13 objectives wins. If one person ticks an objective, the other may not tick it. Prologue cutscenes are not skippable. Standard custom progression rules apply. Additional Notes: The main competitive gamemode and form of Celeste bingo. Highly conducive to strategy.",
         notes:"The main competitive gamemode and form of Celeste bingo. Highly conducive to strategy.",
         color:"Black",
         tags:["lockout", "2-player"]
@@ -11,7 +11,7 @@ let data = [
     {
         name: "Old Longo (Blackout)",
         credit: "Generator created by April, with input from various others",
-        desciption: "A blackout version of the original longo generator. Standard lockout rules apply.",
+        description: "A blackout version of the original longo generator. Standard lockout rules apply.",
         color:"Blue",
         tags:["blackout", "solo", "teams"]
     },
@@ -19,7 +19,7 @@ let data = [
     {
         name: "Minesweeper Bingo",
         credit: "Suggested by gnommis, metagloria, crab, oolimry, Cirion independently. Programmed/finalized by Cirion",
-        desciption: "Teams have to tick all objectives that aren't bombs, while avoiding completing bomb objectives, players are required to tick all completed objectives. Except if the objective is known to be a bomb and required to complete an objective that is known to not be a bomb. Suggested size is 7x7 with 13 mines for 2+ players.",
+        description: "Teams have to tick all objectives that aren't bombs, while avoiding completing bomb objectives, players are required to tick all completed objectives. Except if the objective is known to be a bomb and required to complete an objective that is known to not be a bomb. Suggested size is 7x7 with 13 mines for 2+ players.",
         color:"Green",
         external_links:[{
             name:"Bookmark",
@@ -31,7 +31,7 @@ let data = [
     {
         name: "Fullgo",
         credit: "Epyc",
-        desciption: "A variant of lockout bingo in which every square requires you to complete an extremely long objective from an entirely new save file. After completing and ticking an objective, that save file must be deleted. First to 13 wins.",
+        description: "A variant of lockout bingo in which every square requires you to complete an extremely long objective from an entirely new save file. After completing and ticking an objective, that save file must be deleted. First to 13 wins.",
         color:"Red",
         external_links:[{
             name:"Generator",
