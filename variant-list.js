04ebead715fe508de9b7672ea5cbc7df3b5a139b
cirion02 <34488326+cirion02@users.noreply.github.com>
2023-10-22 21:22:19 +0200

    Add example variants

---
+++ variant-list.js
@@ -0,0 +1,30 @@
+let data = [
+    {
+        name: "Lockout",
+        credit: "Initial draft made by Flesh, updated/maintained by Tim, jacks, marlin, truegen, amber, Spirity, aco, crab, and others.",
+        desciption: "Two players compete to finish a board of 25 squares. First to tick 13 objectives wins. If one person ticks an objective, the other may not tick it. Prologue cutscenes are not skippable. Standard custom progression rules apply. Additional Notes: The main competitive gamemode and form of Celeste bingo. Highly conducive to strategy.",
+        notes:"The main competitive gamemode and form of Celeste bingo. Highly conducive to strategy.",
+        color:"Black",
+        tags:["lockout", "2-player"]
+    },
+
+    {
+        name: "Old Longo (Blackout)",
+        credit: "Generator created by April, with input from various others",
+        desciption: "A blackout version of the original longo generator. Standard lockout rules apply.",
+        color:"Blue",
+        tags:["blackout", "solo", "teams"]
+    },
+
+    {
+        name: "Minesweeper Bingo",
+        credit: "Suggested by gnommis, metagloria, crab, oolimry, Cirion independently. Programmed/finalized by Cirion",
+        desciption: "Teams have to tick all objectives that aren't bombs, while avoiding completing bomb objectives, players are required to tick all completed objectives. Except if the objective is known to be a bomb and required to complete an objective that is known to not be a bomb. Suggested size is 7x7 with 13 mines for 2+ players.",
+        color:"Green",
+        external_links:[{
+            name:"Bookmark",
+            link:"https://discord.com/channels/529677942393929749/689529939975864453/1122479916244545566",
+        }],
+        tags:["blackout", "solo", "teams"]
+    },
+]
